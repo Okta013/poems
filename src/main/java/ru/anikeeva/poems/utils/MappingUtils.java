74a37a67f0a5d3ad31ae4b4f7a785c2d590400bd
okta013 <sindorayka@yandex.ru>
2024-08-08 09:14:11 +0300

    add mappingUtils

---
+++ src/main/java/ru/anikeeva/poems/utils/MappingUtils.java
@@ -0,0 +1,64 @@
+package ru.anikeeva.poems.utils;
+
+import org.springframework.stereotype.Component;
+import ru.anikeeva.poems.dtos.PoemDTO;
+import ru.anikeeva.poems.dtos.RoleDTO;
+import ru.anikeeva.poems.dtos.UserDTO;
+import ru.anikeeva.poems.entities.Poem;
+import ru.anikeeva.poems.entities.Role;
+import ru.anikeeva.poems.entities.User;
+
+@Component
+public class MappingUtils {
+    public UserDTO mapToUserDTO(User user) {
+        UserDTO dto = new UserDTO();
+        dto.setId(user.getId());
+        dto.setUserName(user.getUserName());
+        dto.setPassword(user.getPassword());
+        dto.setFullName(user.getFullName());
+        dto.setEmail(user.getEmail());
+        return dto;
+    }
+
+    public User mapToUser(UserDTO dto) {
+        User user = new User();
+        user.setId(dto.getId());
+        user.setUserName(dto.getUserName());
+        user.setPassword(dto.getPassword());
+        user.setFullName(dto.getFullName());
+        user.setEmail(dto.getEmail());
+        return user;
+    }
+
+    public PoemDTO mapToPoemDTO(Poem poem) {
+        PoemDTO dto = new PoemDTO();
+        dto.setId(poem.getId());
+        dto.setName(poem.getName());
+        dto.setAuthor(poem.getAuthor());
+        dto.setContent(poem.getContent());
+        return dto;
+    }
+
+    public Poem mapToPoem(PoemDTO dto) {
+        Poem poem = new Poem();
+        poem.setId(dto.getId());
+        poem.setName(dto.getName());
+        poem.setAuthor(dto.getAuthor());
+        poem.setContent(dto.getContent());
+        return poem;
+    }
+
+    public RoleDTO mapToRoleDTO(Role role) {
+        RoleDTO dto = new RoleDTO();
+        dto.setId(role.getId());
+        dto.setName(role.getName());
+        return dto;
+    }
+
+    public Role mapToRole(RoleDTO dto) {
+        Role role = new Role();
+        role.setId(dto.getId());
+        role.setName(dto.getName());
+        return role;
+    }
+}
